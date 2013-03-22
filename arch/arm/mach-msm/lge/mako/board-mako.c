7f0f6c6b8bdee705f87b0dca3334a4e2bbc8384c
Manu Gautam <mgautam@codeaurora.org>
2013-03-22 16:59:10 +0530

    USB: ehci-msm-hsic: Apply PHY lockup workarounds only for affected cores
    Some old version HSIC PHY has hardware BUG where due to a race condition
    between h/w initiating SOF (start of frames) and finishing port reset
    can cause PHY lockup. This may result in enumeration failure. Similar
    race conditions exist for resume and suspend scenarios as well.
    Currently driver is enabling software workarounds for the same on all
    hardware revisions. As these BUGs are fixed on latest HSIC PHYs, make
    the workaround selection configurable using DT or board files and enable
    these only for the affected targets.
    
    CRs-fixed: 471128
    Change-Id: I37e88bdff1f664f8782d1bc5125a4fee1b3ea78d
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>
    
    Conflicts:
    
    	Documentation/devicetree/bindings/usb/msm-ehci-hsic.txt
    	arch/arm/boot/dts/msm9625-v1.dtsi
    	arch/arm/boot/dts/msm9625.dtsi
    	drivers/usb/host/ehci-msm-hsic.c

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -790,6 +790,7 @@ static struct msm_hsic_host_platform_data msm_hsic_pdata = {
 	.log2_irq_thresh	= 5,
 	.strobe			= 88,
 	.data			= 89,
+	.phy_sof_workaround     = true,
 	.bus_scale_table	= &hsic_bus_scale_pdata,
 };
 #else
