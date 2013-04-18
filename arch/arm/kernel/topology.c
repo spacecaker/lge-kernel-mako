1f52aed286d87f152805ade20068edac2262c757
hellsgod <hellsgod@gmx.ch>
2013-04-18 09:08:32 +0200

    ARM: sched: clear SD_SHARE_POWERLINE
    The ARM platforms take advantage of packing small tasks on few cores.
    This is true even when the cores of a cluster can't be power gated
    independantly. So we clear SD_SHARE_POWERDOMAIN at MC and CPU level.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -53,7 +53,6 @@
 
 struct cputopo_arm cpu_topology[NR_CPUS];
 
-
 /*
  * cpu power scale management
  * a per cpu data structure should be better because each cpu is mainly
@@ -113,6 +112,15 @@ int arch_sd_sibling_asym_packing(void)
 /*
  * default topology function
  */
+int arch_sd_local_flags(int level)
+{
+	/* Powergate at threading level doesn't make sense */
+	if (level & SD_SHARE_CPUPOWER)
+		return 1*SD_SHARE_POWERDOMAIN;
+
+	return 0*SD_SHARE_POWERDOMAIN;
+}
+
 const struct cpumask *cpu_coregroup_mask(int cpu)
 {
 	return &cpu_topology[cpu].core_sibling;
