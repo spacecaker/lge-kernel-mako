f99060fea235eca99f159133e76606c7377ad588
hellsgod <hellsgod@gmx.ch>
2013-05-12 05:00:55 +0200

    cpufreq: boost the sampling rate on touch event
    For better Ux responsiveness ondemand sampling rate
    needs to be 20ms. But, a 20ms sampling rate increases power
    consumption. So, boost the sampling rate to 20ms on every
    touch event for 2.5 ms and later reset to default rate.
    Also, change sampling down factor proportional to the
    sampling rate.
    
    Change-Id: I111b1cf3b8ed133347149afc34d329d0384ecfcb
    Signed-off-by: Narayanan Gopalakrishnan <nargop@codeaurora.org>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -34,6 +34,7 @@
 #define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(10)
 #define DEF_FREQUENCY_UP_THRESHOLD		(90)
 #define DEF_SAMPLING_DOWN_FACTOR		(4)
+#define BOOSTED_SAMPLING_DOWN_FACTOR		(10)
 #define MAX_SAMPLING_DOWN_FACTOR		(100000)
 #define MICRO_FREQUENCY_DOWN_DIFFERENTIAL	(3)
 #define MICRO_FREQUENCY_UP_THRESHOLD		(95)
@@ -59,6 +60,7 @@ u64 freq_boosted_time;
 
 static unsigned int min_sampling_rate;
 #define DEFAULT_SAMPLING_RATE			(50000)
+#define BOOSTED_SAMPLING_RATE			(20000)
 #define LATENCY_MULTIPLIER			(1000)
 #define MIN_LATENCY_MULTIPLIER			(100)
 #define TRANSITION_LATENCY_LIMIT		(10 * 1000 * 1000)
@@ -470,6 +472,11 @@ static ssize_t store_boostpulse(struct kobject *kobj, struct attribute *attr,
 
 	dbs_tuners_ins.boosted = 1;
 	freq_boosted_time = ktime_to_us(ktime_get());
+
+	if (sampling_rate_boosted) {
+		sampling_rate_boosted = 0;
+		dbs_tuners_ins.sampling_rate = current_sampling_rate;
+	}
 	return count;
 }
 
@@ -494,6 +501,7 @@ static ssize_t store_sampling_rate(struct kobject *a, struct attribute *b,
 	if (ret != 1)
 		return -EINVAL;
 	update_sampling_rate(input);
+	current_sampling_rate = dbs_tuners_ins.sampling_rate;
 	return count;
 }
 
@@ -852,6 +860,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 	this_dbs_info->freq_lo = 0;
 	policy = this_dbs_info->cur_policy;
 	j_dbs_info = &per_cpu(od_cpu_dbs_info, j);
+
 	/* Only core0 controls the boost */
 	if (dbs_tuners_ins.boosted && policy->cpu == 0) {
 		if (ktime_to_us(ktime_get()) - freq_boosted_time >=
@@ -859,6 +868,17 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 			dbs_tuners_ins.boosted = 0;
 		}
 	}
+
+	/* Only core0 controls the timer_rate */
+	if (sampling_rate_boosted && policy->cpu == 0) {
+		if (ktime_to_us(ktime_get()) - sampling_rate_boosted_time >=
+					TIMER_RATE_BOOST_TIME) {
+
+			dbs_tuners_ins.sampling_rate = current_sampling_rate;
+			sampling_rate_boosted = 0;
+		}
+	}
+
 	/*
 	 * Every sampling_rate, we check, if current idle time is less
 	 * than 20% (default), then we try to increase frequency
@@ -973,7 +993,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 
 	j_dbs_info->load_at_prev_sample = load_at_max_freq;
 
-	if (min_sampling_rate < DEFAULT_SAMPLING_RATE)
+	if (dbs_tuners_ins.sampling_rate < DEFAULT_SAMPLING_RATE)
 		cpufreq_notify_utilization(policy, avg_load_at_max_freq);
 	else
 		cpufreq_notify_utilization(policy, load_at_max_freq);
@@ -981,9 +1001,17 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 	/* Check for frequency increase */
 	if (max_load_freq > dbs_tuners_ins.up_threshold * policy->cur) {
 		/* If switching to max speed, apply sampling_down_factor */
-		if (policy->cur < policy->max)
-			this_dbs_info->rate_mult =
-				dbs_tuners_ins.sampling_down_factor;
+		if (policy->cur < policy->max) {
+			if (sampling_rate_boosted &&
+				(dbs_tuners_ins.sampling_down_factor <
+					BOOSTED_SAMPLING_DOWN_FACTOR)) {
+				this_dbs_info->rate_mult =
+					BOOSTED_SAMPLING_DOWN_FACTOR;
+			} else {
+				this_dbs_info->rate_mult =
+					dbs_tuners_ins.sampling_down_factor;
+			}
+		}
 		dbs_freq_increase(policy, policy->max);
 		return;
 	}
@@ -1198,8 +1226,15 @@ static void dbs_input_event(struct input_handle *handle, unsigned int type,
 		return;
 	}
 
+	if (current_sampling_rate > BOOSTED_SAMPLING_RATE) {
+		dbs_tuners_ins.sampling_rate = BOOSTED_SAMPLING_RATE;
+		sampling_rate_boosted_time = ktime_to_us(ktime_get());
+		sampling_rate_boosted = 1;
+	}
+
 	for_each_online_cpu(i)
-		queue_work_on(i, input_wq, &per_cpu(dbs_refresh_work, i).work);
+		queue_work_on(i, input_wq, &per_cpu(dbs_refresh_work, i).work);	
+	}
 }
 
 static int dbs_input_connect(struct input_handler *handler,
