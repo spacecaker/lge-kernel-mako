ed993950f3b37b960746ad8ca174211f66f9b560
hellsgod <hellsgod@gmx.ch>
2013-07-08 00:46:50 +0200

    Tune hyper a little bit (=

--- drivers/cpufreq/cpufreq_hyper.c
+++ drivers/cpufreq/cpufreq_hyper.c
@@ -49,7 +49,7 @@
 #define DEFAULT_FREQ_BOOST_TIME			(500000)
 #define MAX_FREQ_BOOST_TIME			(5000000)
 #define UP_THRESHOLD_AT_MIN_FREQ		(40)
-#define FREQ_FOR_RESPONSIVENESS			(200000)
+#define FREQ_FOR_RESPONSIVENESS			(540000)
 
 static u64 hyper_freq_boosted_time;
 
@@ -154,7 +154,7 @@ static struct dbs_tuners {
 	.ignore_nice = 0,
 	.powersave_bias = 0,
 	.freq_boost_time = DEFAULT_FREQ_BOOST_TIME,
-	.boostfreq = 1200000,
+	.boostfreq = 1242000,
 	.freq_step = FREQ_STEP,
 	.freq_responsiveness = FREQ_FOR_RESPONSIVENESS
 };
