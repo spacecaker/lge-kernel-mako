2bb50b3f70b34ab77be69c1a5fac001e1da27a90
hellsgod <hellsgod@gmx.ch>
2013-07-14 03:14:49 +0200

    Add SMARTMAX Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -408,6 +408,9 @@ extern struct cpufreq_governor cpufreq_gov_lazy;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
 extern struct cpufreq_governor cpufreq_gov_hyper;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_hyper)  
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartmax) 
 #endif
 
 
